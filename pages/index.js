1a0f3c7ad32ef78d808689814807b9054ba36e14
nikhil23-Rao <vinay23.rao@gmail.com>
2025-10-14 16:41:30 -0700

    comment out pop ups

--- pages/index.js
+++ pages/index.js
@@ -117,10 +117,10 @@ export default function Home() {
           color: colors.text,
         }}
       >
-        <div style={{border: `2px solid ${colors.border}`, borderRadius: 20}}>
+        {/* <div style={{border: `2px solid ${colors.border}`, borderRadius: 20}}>
         <p className="text-sm mb-4" style={{ color: colors.subtext, textAlign: "center", padding: 5, fontSize: 13, marginTop: 5, marginBottom: 7.5 }}>
               Learn more about participating in the 2026 SRVSO by clicking <u><a href="https://forms.gle/NMDMqMLGNnXRTBUS8" target='_blank' style={{color: "#5A93DB"}}>here</a></u> to sign up for our Information Nights!
-            </p></div>
+            </p></div> */}
         
         <div
           className="flex flex-row items-start max-w-6xl mx-auto mt-20 sm:mt-8 gap-x-20 sm:flex-col sm:gap-x-0 sm:gap-y-10 sm:items-center"
@@ -474,7 +474,7 @@ export default function Home() {
           </div>
         </section>
 
-        {showPopup && (
+        {/* {showPopup && (
         <div
           className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
           onClick={closePopup}
@@ -496,7 +496,7 @@ export default function Home() {
             />
           </div>
         </div>
-      )}
+      )} */}
       </div>
     </div>
   );
